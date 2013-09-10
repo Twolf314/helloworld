332d698aa72e259a9bb3da5fdd69c5b733d633f4
Donald Lee <kndonlee@gmail.com>
2013-09-10 19:57:20 -0400

    donald added 2 lines

--- ex1.rb
+++ ex1.rb
@@ -5,3 +5,6 @@ puts "This is fun."
 puts 'Yay! Printing'
 puts "I'd much rather you 'not'."
 puts 'I "said" do not touch this.'
+
+puts "donald added some extra lines to ex1.rb"
+puts "donald added more stuff"
