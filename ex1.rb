0e56d17a6868b46e7e573e39609da5a3c838b22d
Donald Lee <kndonlee@gmail.com>
2013-09-10 19:58:17 -0400

    formatting changes

--- ex1.rb
+++ ex1.rb
@@ -6,6 +6,8 @@ puts 'Yay! Printing'
 puts "I'd much rather you 'not'."
 puts 'I "said" do not touch this.'
 
+puts
+
 puts "donald added some extra lines to ex1.rb"
 puts "donald added more stuff"
 
