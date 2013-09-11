412500d999d56f1ab55a0da25683f9b246ef10fc
Twolf314 <twolf314@Leslies-MacBook-Air.local>
2013-09-10 22:28:20 -0400

    recommitted

--- ex3.rb
+++ ex3.rb
@@ -12,7 +12,7 @@ puts "Is is true that 3 + 2 < 5 -7?"
 puts 3 + 2 < 5 - 7
 
 puts "What is 3 + 2?", 3 + 2
-puts "What is 5 - 7?" 5 - 7
+puts "What is 5 - 7?", 5 - 7
 
 puts "Oh, that's why it's false."
 
