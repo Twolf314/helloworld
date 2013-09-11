5d2160e2626d945b0e99bb2435cc0e9c062ad017
Donald Lee <donald.lee@nasdaqomx.com>
2013-09-10 22:39:57 -0400

    broke ex4

--- ex4.rb
+++ ex4.rb
@@ -1,5 +1,5 @@
 cars = 100
-space_in_a_car = 4.0
+space_in_a_car 4.0
 drivers = 30
 passengers = 90
 cars_not_driven = cars - drivers
@@ -9,7 +9,7 @@ average_passengers_per_car = passengers / cars_driven
 
 puts "There are #{cars} cars available."
 puts "There are only #{drivers} drivers available."
-puts "There will be #{cars_not_driven} empty cars today."
+puts "There will be #{cars_not_driven} empty cars today.
 puts "We can transport #{carpool_capacity} people today."
 puts "We have #{passengers} passengers to carpool today."
 puts "We need to put about #{average_passengers_per_car} in each car."
