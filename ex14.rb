a397d9d06ec3dacd1a7f7c7851a269264b3af2ab
Ecks <ecks@ecks-120-1134.(none)>
2013-09-14 21:15:38 -0400

    fixed.

--- ex14.rb
+++ ex14.rb
@@ -1,7 +1,7 @@
 user = ARGV.first 
 prompt = '> '
 
-puts "Hi #{user}, I'm the #{0} script."
+puts "Hi #{user}, I'm the #{$0} script."
 puts "I'd like to ask you a few questions."
 puts "Do you like me #{user}?"
 print prompt
