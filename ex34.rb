106b886663a4a5c7ee8e7dbfd0b24ce4e9811a4d
Ecks <ecks@ecks-120-1134.(none)>
2013-09-29 15:30:44 -0400

    exercise 34

---
+++ ex34.rb
@@ -0,0 +1,10 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+puts "The animal at 1 is the second animal and is a bear."
+puts "The 3rd animal is the animal at 2 and is a peacock."
+puts "The first animal is the animal at 0 and is a bear."
+puts "The animal at 3 is the fourth animal and is a kangaroo."
+puts "The fifth animal is the animal at four and is a whale."
+puts "The animal at two is the third animal and is a peacock."
+puts "The sixth animal is the animal at 5 and is a platypus."
+puts "The animal at 4 is the fifth animal and is a whale."
+
